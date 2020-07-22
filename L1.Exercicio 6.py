007bad9eba03ce8a7181f092a1f4ec5883a4cea7
Eric <68623578+Ericxk09@users.noreply.github.com>
2020-07-21 23:14:41 -0300

    L1. Exercicio 6
    Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média
    esperada para a viagem.

---
+++ L1.Exercicio 6.py
@@ -0,0 +1,6 @@
+distancia = float(input(' Digite a distancia: '))
+velocidade = float(input(' Digite a Media de Velocidade: '))
+
+tempo_viagem = distancia / velocidade 
+
+print('Tempo de viagem será de aproximadamente: ',tempo_viagem)
