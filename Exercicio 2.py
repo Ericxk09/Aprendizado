b12b2aceb527dfb13c3af539e1029d11d6aa49f7
Eric <68623578+Ericxk09@users.noreply.github.com>
2020-07-31 21:06:27 -0300

    Exercicio 2 - Coursera
    Faça um programa em Python que receba quatro notas, calcule e imprima a média aritmética. Observe o exemplo abaixo:
    
    Exemplo:
    
    Entrada de Dados:
    Digite a primeira nota: 4
    
    Digite a segunda nota: 5
    
    Digite a terceira nota: 6
    
    Digite a quarta nota: 7
    
    Saída de Dados:
    A média aritmética é 5.5

---
+++ Exercicio 2.py
@@ -0,0 +1,6 @@
+note1 = float(input("Digite o valor da nota: "))
+note2 = float(input("Digite o valor da nota: "))
+note3 = float(input("Digite o valor da nota: "))
+note4 = float(input("Digite o valor da nota: "))
+average = (note1 + note2  + note3 + note4)/4
+print("A média aritmética é",average)
