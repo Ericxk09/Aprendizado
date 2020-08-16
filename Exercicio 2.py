717909aefda370b2a028bd54933c89f6263dc4c1
Eric <68623578+Ericxk09@users.noreply.github.com>
2020-08-16 14:23:26 -0300

    Exercicio 2 - Semana 8 - Coursera
    Escreva a função soma_elementos que recebe como parâmetro uma lista com números inteiros e devolve um número inteiro correspondente à soma dos elementos da lista recebida

--- Exercicio 2.py
+++ Exercicio 2.py
@@ -1,37 +1,5 @@
-def width(x):
-    
-    while x > 0:
-        
-        print('#', end = '')
-        x = x - 1
-    
-    
-def height(x):
-    
-    while x > 0:
-        
-        print(' ', end = '')
-        x = x - 1
-
-largura = int(input("Digite a largura: "))
-altura = int(input("Digite a altura: "))
-
-while altura > 0:
-    width(largura)
-    print()
-    altura = altura  - 1
-    while altura > 1:
-        width(1)
-        height(largura-2)
-        width(1)
-        print()
-        altura = altura - 1
-        
-    width(largura)
-    altura = altura - 1
-
-
-
-
-
-
+def soma_elementos(lista):
+    soma = 0
+    for x in lista:
+        soma = soma + x
+    return soma
